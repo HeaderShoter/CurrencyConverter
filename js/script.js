29abeed060e9063f6c7f5288a4fa4bc9293dce51
HeaderShoter <34886197+HeaderShoter@users.noreply.github.com>
2023-08-02 00:37:39 +0200

    JavaScript V2

--- js/script.js
+++ js/script.js
@@ -1,90 +1,87 @@
 console.log("Hello world!");
 
-let sellValue = document.querySelector(".js-sellValue");
-let chargeValue = document.querySelector(".js-chargeValue");
-let sellType = document.querySelector(".js-sellType");
-let chargeType = document.querySelector(".js-chargeType");
+let PLNEUR = 0.22;
+let PLNUSD = 0.25;
+let PLNGBP = 0.19;
+let EURPLN = 4.45;
+let EURUSD = 1.11;
+let EURGBP = 0.86;
+let USDPLN = 4.02;
+let USDEUR = 0.90;
+let USDGBP = 0.78;
+let GBPPLN = 5.16;
+let GBPEUR = 1.16;
+let GBPUSD = 1.28;
 
-sellValue.addEventListener("input", () => {
-  if (sellType.value === chargeType.value) {
-    chargeValue.value = (+sellValue.value).toFixed(2);
+let inputSell = document.querySelector(".js-inputSell");
+let inputCharge = document.querySelector(".js-inputCharge");
+let selectSell = document.querySelector(".js-selectSell");
+let selectCharge = document.querySelector(".js-selectCharge");
+
+function setExchangeRate() {
+  let exchange = selectSell.value + selectCharge.value;
+  switch (exchange) {
+    case 'PLNEUR': window.exchangeRate = PLNEUR;
+      break;
+    case 'PLNUSD': window.exchangeRate = PLNUSD;
+      break;
+    case 'PLNGBP': window.exchangeRate = PLNGBP;
+      break;
+    case 'EURPLN': window.exchangeRate = EURPLN;
+      break;
+    case 'EURUSD': window.exchangeRate = EURUSD;
+      break;
+    case 'EURGBP': window.exchangeRate = EURGBP;
+      break;
+    case 'USDPLN': window.exchangeRate = USDPLN;
+      break;
+    case 'USDEUR': window.exchangeRate = USDEUR;
+      break;
+    case 'USDGBP': window.exchangeRate = USDGBP;
+      break;
+    case 'GBPPLN': window.exchangeRate = GBPPLN;
+      break;
+    case 'GBPEUR': window.exchangeRate = GBPEUR;
+      break;
+    case 'GBPUSD': window.exchangeRate = GBPUSD;
+      break;
+    default: window.exchangeRate = 1;
   }
-  if (sellType.value === `PLN` && chargeType.value === `EUR`) {
-    chargeValue.value = (+sellValue.value * 0.22).toFixed(2);
-  }
-  if (sellType.value === `PLN` && chargeType.value === `USD`) {
-    chargeValue.value = (+sellValue.value * 0.25).toFixed(2);
-  }
-  if (sellType.value === `PLN` && chargeType.value === `GBP`) {
-    chargeValue.value = (+sellValue.value * 0.19).toFixed(2);
-  }
-  if (sellType.value === `EUR` && chargeType.value === `PLN`) {
-    chargeValue.value = (+sellValue.value * 4.45).toFixed(2);
-  }
-  if (sellType.value === `EUR` && chargeType.value === `USD`) {
-    chargeValue.value = (+sellValue.value * 1.11).toFixed(2);
-  }
-  if (sellType.value === `EUR` && chargeType.value === `GBP`) {
-    chargeValue.value = (+sellValue.value * 0.86).toFixed(2);
-  }
-  if (sellType.value === `USD` && chargeType.value === `PLN`) {
-    chargeValue.value = (+sellValue.value * 4.02).toFixed(2);
-  }
-  if (sellType.value === `USD` && chargeType.value === `EUR`) {
-    chargeValue.value = (+sellValue.value * 0.90).toFixed(2);
-  }
-  if (sellType.value === `USD` && chargeType.value === `GBP`) {
-    chargeValue.value = (+sellValue.value * 0.78).toFixed(2);
-  }
-  if (sellType.value === `GBP` && chargeType.value === `PLN`) {
-    chargeValue.value = (+sellValue.value * 5.16).toFixed(2);
-  }
-  if (sellType.value === `GBP` && chargeType.value === `EUR`) {
-    chargeValue.value = (+sellValue.value * 1.16).toFixed(2);
-  }
-  if (sellType.value === `GBP` && chargeType.value === `USD`) {
-    chargeValue.value = (+sellValue.value * 1.28).toFixed(2);
-  }
+};
+
+function sell() {
+  setExchangeRate();
+  let sellValue = +inputSell.value;
+  inputCharge.value = (sellValue * exchangeRate).toFixed(2);
+};
+
+function charge() {
+  setExchangeRate();
+  let chargeValue = +inputCharge.value;
+  inputSell.value = (chargeValue / exchangeRate).toFixed(2);
+};
+
+function inputActive() {
+  if (document.activeElement === inputSell) { window.inputFocus = 'sell' };
+  if (document.activeElement === inputCharge) { window.inputFocus = 'charge' };
+};
+
+inputSell.addEventListener("input", () => {
+  sell();
+  inputActive();
 });
 
-chargeValue.addEventListener("input", () => {
-  if (sellType.value === chargeType.value) {
-    sellValue.value = (+chargeValue.value).toFixed(2);
-  }
-  if (sellType.value === `PLN` && chargeType.value === `EUR`) {
-    sellValue.value = (+chargeValue.value / 0.22).toFixed(2);
-  }
-  if (sellType.value === `PLN` && chargeType.value === `USD`) {
-    sellValue.value = (+chargeValue.value / 0.25).toFixed(2);
-  }
-  if (sellType.value === `PLN` && chargeType.value === `GBP`) {
-    sellValue.value = (+chargeValue.value / 0.19).toFixed(2);
-  }
-  if (sellType.value === `EUR` && chargeType.value === `PLN`) {
-    sellValue.value = (+chargeValue.value / 4.45).toFixed(2);
-  }
-  if (sellType.value === `EUR` && chargeType.value === `USD`) {
-    sellValue.value = (+chargeValue.value / 1.11).toFixed(2);
-  }
-  if (sellType.value === `EUR` && chargeType.value === `GBP`) {
-    sellValue.value = (+chargeValue.value / 0.86).toFixed(2);
-  }
-  if (sellType.value === `USD` && chargeType.value === `PLN`) {
-    sellValue.value = (+chargeValue.value / 4.02).toFixed(2);
-  }
-  if (sellType.value === `USD` && chargeType.value === `EUR`) {
-    sellValue.value = (+chargeValue.value / 0.90).toFixed(2);
-  }
-  if (sellType.value === `USD` && chargeType.value === `GBP`) {
-    sellValue.value = (+chargeValue.value / 0.78).toFixed(2);
-  }
-  if (sellType.value === `GBP` && chargeType.value === `PLN`) {
-    sellValue.value = (+chargeValue.value / 5.16).toFixed(2);
-  }
-  if (sellType.value === `GBP` && chargeType.value === `EUR`) {
-    sellValue.value = (+chargeValue.value / 1.16).toFixed(2);
-  }
-  if (sellType.value === `GBP` && chargeType.value === `USD`) {
-    sellValue.value = (+chargeValue.value / 1.28).toFixed(2);
-  }
+inputCharge.addEventListener("input", () => {
+  charge();
+  inputActive();
+});
+
+selectSell.addEventListener("change", () => {
+  if (inputFocus === 'sell') { sell(); }
+  if (inputFocus === 'charge') { charge(); }
+});
+
+selectCharge.addEventListener("change", () => {
+  if (inputFocus === 'sell') { sell(); }
+  if (inputFocus === 'charge') { charge(); }
 });
